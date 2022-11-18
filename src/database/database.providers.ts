4d8eab6ff2d6992ead90a7f99f200569ebe42576
Rui Augusto <rui.augusto.drumond@gmail.com>
2022-11-18 15:11:22 -0300

    consertando db config

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,11 +6,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '123456',
+        database: 'desafio_node_nest_v01',
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
         ],
