04e43bf79ab85db224614340ac83e6889e760e1d
Rui Augusto <rui.augusto.drumond@gmail.com>
2022-11-18 14:26:38 -0300

    corrigindo db

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,17 +1,16 @@
 import { DataSource } from 'typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 export const databaseProviders = [
   {
-    provide: 'DB_CONNECTION',
-    useFactory: async (configService: ConfigService) => {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: configService.get('DB_HOST', 'localhost'),
-        port: configService.get('DB_PORT', 3306),
-        username: configService.get('DB_USERNAME', 'root'),
-        password: configService.get('DB_PASSWORD', '123456'),
-        database: configService.get('DB_DATABASE', 'desafio_node_nest_v01'),
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '123456',
+        database: 'desafio_node_nest_v01',
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
         ],
@@ -21,4 +20,4 @@ export const databaseProviders = [
       return dataSource.initialize();
     },
   },
-];
+]; 
