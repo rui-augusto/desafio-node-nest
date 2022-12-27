aa12d465f2e72dd5f60eba3bc965753432dd4a5d
rui-augusto <rui.augusto.drumond@gmail.com>
2022-12-27 14:38:23 -0300

    add: create-user unit testing

--- src/user/test/user.controller.spec.ts
+++ src/user/test/user.controller.spec.ts
@@ -1,3 +1,4 @@
+import { CreateUserDto } from './../dto/create-user.dto';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from '../user.controller';
 import { UserService } from '../user.service';
@@ -14,6 +15,13 @@ const UserList: User[] = [
     })
 ]
 
+const CreateUser: User = new User({
+    firstName: 'fn',
+    lastName: 'ln',
+    password: "password",
+    email: 'fn.ln@dreamlabs.com'
+});
+
 describe ('UserController', () => {
     let userController: UserController;
     let userService: UserService;
@@ -29,7 +37,7 @@ describe ('UserController', () => {
                         findAll: jest.fn().mockResolvedValue(UserList), 
                         findOne: jest.fn(),
                         login: jest.fn(),
-                        create: jest.fn(),
+                        create: jest.fn().mockResolvedValue(CreateUser), 
                         update: jest.fn(),
                         remove: jest.fn(),
                     },
@@ -51,6 +59,23 @@ describe ('UserController', () => {
         it('should return a list of users', async () => {
             const result = await userController.index();
             expect(result).toEqual(UserList);
+            expect(typeof result).toEqual('object');
         });
      });
+
+     // * confer if in the controller the return value of create should be a User
+     describe('create', () => {
+        it('should create a new user', async () => {
+
+            const body: CreateUserDto = {
+                firstName: "fn",
+                lastName: "sn",
+                password: "password",
+                email: "email@email.com"
+            }
+
+            const result = await userController.create(body);
+            expect(result).toEqual(`${CreateUser.email} registered`);
+        });
+     })
 });
