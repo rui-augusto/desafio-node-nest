ac7e50f45fb90fd31d1e3ebd529267e729df59ad
rui-augusto <rui.augusto.drumond@gmail.com>
2023-01-12 09:42:39 -0300

    updating dependencies versions

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from "./user/user.module";
-import { AuthModule } from './user/auth/auth.module.';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
